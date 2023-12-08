5b102a8f56102eb1ac5fc85920cfbbed328169aa
tamilarasi <tamilarasi.shanmugasundaram@ideas2it.com>
2023-12-08 18:48:02 +0530

    Intial Commit

---
+++ src/main/java/com/core/hos/HosApplication.java
@@ -0,0 +1,13 @@
+package com.core.hos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosApplication.class, args);
+	}
+
+}
