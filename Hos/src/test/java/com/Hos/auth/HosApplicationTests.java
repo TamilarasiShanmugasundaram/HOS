86f235d43e81c3a52c5fd6e6d3e1208f545f9a2b
nandhakumar7 <nandhakumar.karthikeyan@ideas2it.com>
2023-12-09 13:52:03 +0530

    Intial setup Commit

---
+++ Hos/src/test/java/com/Hos/auth/HosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hos.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
