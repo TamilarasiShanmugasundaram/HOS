9e928d7fd5f21479bbc50b76efc836ea5093ee2d
nandhakumar7 <nandhakumar.karthikeyan@ideas2it.com>
2023-12-09 13:52:03 +0530

    Intial setup Commit

---
+++ Hos/src/main/java/com/Hos/HosApplication.java
@@ -0,0 +1,14 @@
+package com.Hos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosApplication.class, args);
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# HOS
+
+APPLICATION RUN COMMANDS:
+1.mvn clean install
+2.mvn spring-boot:run
+
+Test Application Using This API's:
+
+Request Type: GET
+
+   TestRequestController: http://localhost:8099/request/hello
+   TestCommunityController: http://localhost:8099/community/hello
+
+
